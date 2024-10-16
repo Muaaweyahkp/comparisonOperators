09562b37ed699e0c1a7589d98d3976ad6f3b287d
Muaaweyah Ibn Noufal K.P <muaaweyahkp@gmail.com>
2024-10-16 22:55:04 +0530

    camparison operators

---
+++ comparison-oper.js
@@ -0,0 +1,19 @@
+// comparison operators
+
+// == double equal to
+console.log(3==3);
+
+//  != not equal to
+console.log(3 != 3);
+
+// > greater
+console.log(3>1);
+
+//  < lesser
+console.log(3<2);
+
+//  >= greater than or equal to
+console.log(3>=5);
+
+//  <= less than or equal to 
+console.log(3<=4);
